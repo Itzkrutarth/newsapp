e4039124e985f4a3f089a16f8338238c15b428d8
Krutarth Patel <kishanpatel9702@gmail.com>
2024-05-01 18:03:00 +0530

    Navbar Creted

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import "./App.css"
 import React, { Component } from "react"
+import { Navbar } from "./components/navbar"
+import News from "./components/news"
 
 export default class App extends Component {
 	render() {
-		return <div>Hello Krutarth..</div>
+		return (
+			<div>
+				<Navbar />
+				<News />
+			</div>
+		)
 	}
 }
